6ada8aed3727151095a0eba7560d899787ac5522
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2024-08-15 11:43:38 +0200

    Remove unused cases from Delimiters, make it @internal

--- src/Parser/Delimiters.php
+++ src/Parser/Delimiters.php
@@ -4,18 +4,17 @@ declare(strict_types=1);
 
 namespace Eventjet\Ausdruck\Parser;
 
+/**
+ * @psalm-internal Eventjet\Ausdruck\Parser
+ */
 enum Delimiters
 {
-    case Parentheses;
-    case Brackets;
     case CurlyBraces;
     case AngleBrackets;
 
     public function start(): string
     {
         return match ($this) {
-            self::Parentheses => '(',
-            self::Brackets => '[',
             self::CurlyBraces => '{',
             self::AngleBrackets => '<',
         };
@@ -24,8 +23,6 @@ enum Delimiters
     public function end(): string
     {
         return match ($this) {
-            self::Parentheses => ')',
-            self::Brackets => ']',
             self::CurlyBraces => '}',
             self::AngleBrackets => '>',
         };
