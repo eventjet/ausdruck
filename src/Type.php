6354ad3c93debde1c4a0df3ead4130c5cdaa29ad
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2025-09-29 11:13:39 +0200

    Delete an unused if statement

--- src/Type.php
+++ src/Type.php
@@ -172,9 +172,6 @@ final class Type implements Stringable
      */
     public function assert(mixed $value): mixed
     {
-        if ($value === [] && $this->name === 'map') {
-            return $value;
-        }
         $valueType = self::fromValue($value);
         return $valueType->isSubtypeOf($this)
             ? $value
